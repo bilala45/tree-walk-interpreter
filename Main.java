b3bf736b31aa7c03c1a6b4d4f6b11cfd9b76c37e
Bilal Ali <bilalal495@outlook.com>
2022-07-10 17:13:06 -0400

    handle arguments to Main class and initialize Run class to manage inputs

--- Main.java
+++ Main.java
@@ -1,7 +1,17 @@
-package lox_interpreter
+package lox_interpreter;
 
 public class Main {
+
   public static void main(String[] args) {
-    
+    if (args.length > 1) {
+      // exit program - too many arguments
+      System.exit(1);
+    } else if (args.length == 1) {
+      // runs file path passed in as argument
+      Run.runFile(args[0]);
+    } else {
+      // runs interpreter interactively
+      Run.runPrompt();
+    }
   }
 }
---
+++ Run.java
@@ -0,0 +1,14 @@
+package lox_interpreter;
+
+public class Run {
+
+  // method to run file path specified in command line call
+  public static void runFile(String filePath) {
+    System.out.println("runFile working");
+  }
+
+  // method to run interactive prompt for interpreter
+  public static void runPrompt() {
+    System.out.println("runPrompt working");
+  }
+}
