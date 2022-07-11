88d9686422d7de28696e45a609d390a6abee8d3d
Bilal Ali <bilalal495@outlook.com>
2022-07-11 15:35:06 -0400

    Add fields and constructor to generate token object

--- Token.java
+++ Token.java
@@ -2,4 +2,17 @@ package lox_interpreter;
 
 public class Token {
 
+  // type of token
+  private TokenType tokenType;
+  // lexeme associated with token
+  private String lexeme;
+  // tracks line where token was found
+  private int line;
+
+  // constructor
+  public Token(TokenType tokenType, String lexeme, int line) {
+    this.tokenType = tokenType;
+    this.lexeme = lexeme;
+    this.line = line;
+  }
 }
